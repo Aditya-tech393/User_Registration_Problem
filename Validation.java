3666cec54a6c7ea830900fc2aec1e1bb95966ecf
Aditya-tech393 <adityapd2302@gmail.com>
2024-10-06 23:22:13 +0530

    [Aditya]: Checking the entered mobile number by the user

--- Validation.java
+++ Validation.java
@@ -35,8 +35,20 @@ public class Validation {
             System.out.println("Invalid email. It must follow the format abc.xyz@bl.co.in.");
         }
 
+        System.out.print("Enter your mobile number (e.g., 91 9919819801): ");
+        String mobileNumber = scanner.nextLine();
+
+
+        if (isValidMobileNumber(mobileNumber)) {
+            System.out.println("Valid mobile number.");
+        } else {
+            System.out.println("Invalid mobile number. It must follow the format 91 9919819801.");
+        }
+
+
         scanner.close();
     }
+
 
     public static boolean isValidFirstName(String firstName) {
         String regex = "^[A-Z][a-z]{2,}$";
@@ -51,5 +63,9 @@ public class Validation {
     public static boolean isValidEmail(String email) {
         String regex = "^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
         return email.matches(regex);
+    }
+    public static boolean isValidMobileNumber(String mobileNumber) {
+        String regex = "^91\\s[0-9]{10}$";
+        return mobileNumber.matches(regex);
     }
 }
